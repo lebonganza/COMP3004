6852ca61d5f7742c70ca6af8e297a0319b695ee1
lebonganza <ganzayveslebon.gyl@gmail.com>
2018-09-16 23:07:41 -0400

    test: added a test to check if a deck initially has 52 cards

--- src/test/java/core/BlackjackTest.java
+++ src/test/java/core/BlackjackTest.java
@@ -4,8 +4,14 @@ import junit.framework.TestCase;
 
 public class BlackjackTest extends TestCase{
 	
-	public void TestCard() {
+	public void testCard() {
 		Card card = new Card(Suit.values()[0],5);
 		assertEquals(5,card.getValue());
+	}
+	
+	public void testDeck() {
+		Deck deck = new Deck();
+		
+		assertEquals(52, deck.getNumCards());
 	}
 }

4ce0082899339f74a6ac8285123f8c2607fe64ed
lebonganza <ganzayveslebon.gyl@gmail.com>
2018-09-17 11:52:50 -0400

    test: added test to check the value of Jacks Queens and Kings as well as Aces

--- src/test/java/core/BlackjackTest.java
+++ src/test/java/core/BlackjackTest.java
@@ -5,8 +5,17 @@ import junit.framework.TestCase;
 public class BlackjackTest extends TestCase{
 	
 	public void testCard() {
-		Card card = new Card(Suit.values()[0],5);
-		assertEquals(5,card.getValue());
+		Card fiveofClubs = new Card(Suit.C,5);
+		Card kingofClubs = new Card(Suit.C,10);
+		Card queenofDiamonds = new Card(Suit.D,10);
+		Card aceofHearts = new Card(Suit.H,1);
+		Card jackofSpades = new Card(Suit.S,10);
+		
+		assertEquals(5,fiveofClubs.getValue());
+		assertEquals(10,kingofClubs.getValue());
+		assertEquals(10,queenofDiamonds.getValue());
+		assertEquals(1,aceofHearts.getValue());
+		assertEquals(10,jackofSpades.getValue());
 	}
 	
 	public void testDeck() {

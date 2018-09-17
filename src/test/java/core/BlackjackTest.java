37bf19683e044aedcb8e145e59420b79543ebc75
lebonganza <ganzayveslebon.gyl@gmail.com>
2018-09-16 22:30:59 -0400

    test: added a test for the card's value

--- src/test/java/core/BlackjackTest.java
+++ src/test/java/core/BlackjackTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 public class BlackjackTest extends TestCase{
 	
 	public void TestCard() {
-		Card card = new Card();
+		Card card = new Card(5);
+		assertEquals(5,card.getValue());
 	}
 }

a2bc4fd4c3c6e79c6d71d4afe3994a085e1bc8c1
lebonganza <ganzayveslebon.gyl@gmail.com>
2018-09-17 12:01:12 -0400

    test: added a test for the toString method of the cards

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -18,4 +18,52 @@ public class Card {
 	public Suit getSuit() {
 		return this.suit;
 	}
+	
+	public String toString() {
+		
+		String valueStr ="NULL";
+		
+		switch(this.value) {
+			case 2:
+				valueStr = "2";
+				break;
+			case 3:
+				valueStr = "3";
+				break;
+			case 4:
+				valueStr = "4";
+				break;
+			case 5:
+				valueStr = "5";
+				break;
+			case 6:
+				valueStr = "6";
+				break;
+			case 7:
+				valueStr = "7";
+				break;
+			case 8:
+				valueStr = "8";
+				break;
+			case 9:
+				valueStr = "9";
+				break;
+			case 10:
+				valueStr = "10";
+				break;
+			case 11:
+				valueStr = "J";
+				break;
+			case 12:
+				valueStr = "Q";
+				break;
+			case 13:
+				valueStr = "K";
+				break;
+			case 1:
+				valueStr = "A";
+				break;			
+		}
+		return suit.toString() + valueStr;
+	}
 }
--- src/test/java/core/BlackjackTest.java
+++ src/test/java/core/BlackjackTest.java
@@ -4,12 +4,13 @@ import junit.framework.TestCase;
 
 public class BlackjackTest extends TestCase{
 	
+	Card fiveofClubs = new Card(Suit.C,5);
+	Card kingofClubs = new Card(Suit.C,10);
+	Card queenofDiamonds = new Card(Suit.D,10);
+	Card aceofHearts = new Card(Suit.H,1);
+	Card jackofSpades = new Card(Suit.S,10);
+	
 	public void testCard() {
-		Card fiveofClubs = new Card(Suit.C,5);
-		Card kingofClubs = new Card(Suit.C,10);
-		Card queenofDiamonds = new Card(Suit.D,10);
-		Card aceofHearts = new Card(Suit.H,1);
-		Card jackofSpades = new Card(Suit.S,10);
 		
 		assertEquals(5,fiveofClubs.getValue());
 		assertEquals(10,kingofClubs.getValue());
@@ -18,6 +19,10 @@ public class BlackjackTest extends TestCase{
 		assertEquals(10,jackofSpades.getValue());
 	}
 	
+	public void testToString() {
+		assertEquals("C5", fiveofClubs.toString());
+		assertEquals("HA", aceofHearts.toString());
+	}
 	public void testDeck() {
 		Deck deck = new Deck();
 		
